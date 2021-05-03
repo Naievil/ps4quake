f1c1c85f0680b3aa2c873b23e85a198514034328
Ryan Baldwin <ryanbaldwin@Ryans-MBP.attlocal.net>
2021-05-02 23:37:15 -0700

    first commit

---
+++ build.sh
@@ -0,0 +1,3 @@
+make 
+PkgTool.Core pkg_build /Users/ryanbaldwin/ps4dev/hello_world/pkg/pkg.gp4 /Users/ryanbaldwin/ps4dev/hello_world/pkg
+mv /Users/ryanbaldwin/ps4dev/hello_world/pkg/IV0000-BREW00083_00-HELLOWORLD000000.pkg /Volumes/Untitled
